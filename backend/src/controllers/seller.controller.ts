ef2f611764949fdee5ce1e5290d48a8cef786f2d
unknown <pn.giabao9705@gmail.com>
2025-12-01 09:51:48 +0700

    feat: seller service

---
+++ backend/src/controllers/seller.controller.ts
@@ -0,0 +1,23 @@
+import {Request, Response} from 'express';
+import * as sellerService from '../services/seller.service';
+
+export const createProduct = async (req: Request, res: Response) => {
+    try{
+        const sellerId = (req as any).user.id;
+        const result = await sellerService.createProduct(sellerId, req.body);
+        res.status(201).json({success: true, ...result});
+
+    }catch(error: any){
+        res.status(500).json({success: false, message: error.message});
+    }
+};
+
+export const getMyProuducts = async (req: Request, res: Response) => {
+    try{
+        const sellerId = (req as any).user.id;
+        const products = await sellerService.getMyProducts(sellerId);
+        res.status(200).json({success: true, products});    
+    }catch(error: any){
+        res.status(500).json({success: false, message: error.message});
+    }
+};
